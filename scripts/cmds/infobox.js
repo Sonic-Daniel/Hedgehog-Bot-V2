effaaff25ef963c4792cb28e9a174ef7fbbc2d0b
ʬʆʬ Sønïč ʬʆʬ <danielthiam54@gmail.com>
2024-10-23 20:29:11 +0100

    Update and rename antichangeinfobox.js to infobox.js

--- scripts/cmds/infobox.js
+++ scripts/cmds/infobox.js
@@ -2,7 +2,7 @@ const { getStreamFromURL, uploadImgbb } = global.utils;
 
 module.exports = {
 	config: {
-		name: "antichangeinfobox",
+		name: "infobox",
 		version: "1.9",
 		author: "NTKhang",
 		countDown: 5,
